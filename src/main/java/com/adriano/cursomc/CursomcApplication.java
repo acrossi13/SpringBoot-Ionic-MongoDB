5dbad6e2db274cfd7c5df01d00f7d9d7aa78778c
acrossi13 <acrossi.eprom@gmail.com>
2020-09-30 23:42:54 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/adriano/cursomc/CursomcApplication.java
+++ src/main/java/com/adriano/cursomc/CursomcApplication.java
@@ -103,7 +103,7 @@ public class CursomcApplication implements CommandLineRunner{
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 		
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
-		cidadeRepository.saveAll(Arrays.asList(c1,c2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
 		Cliente cli1 = new Cliente(null,"Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		
