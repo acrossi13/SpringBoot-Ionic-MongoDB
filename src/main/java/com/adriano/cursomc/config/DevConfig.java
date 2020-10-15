95e20adbf882ca4eff64ed6691ca24be82ad4d84
acrossi13 <acrossi.eprom@gmail.com>
2020-10-15 11:30:16 -0300

    Criacao de Servico para envio de email via gmail e mailtrap

--- src/main/java/com/adriano/cursomc/config/DevConfig.java
+++ src/main/java/com/adriano/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.adriano.cursomc.services.DBService;
+import com.adriano.cursomc.services.EmailService;
+import com.adriano.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,5 +29,10 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDataBase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService(){
+	return new SmtpEmailService();
 	}
 }
