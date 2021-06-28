ec8d9eb9826163ff0ade0cf087df9ebe9aa56252
acrossi13 <acrossi.eprom@gmail.com>
2021-06-28 15:24:35 -0300

    restricao de conteudo: cliente recupera ele mesmo

--- src/main/java/com/adriano/cursomc/services/ClienteService.java
+++ src/main/java/com/adriano/cursomc/services/ClienteService.java
@@ -15,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.adriano.cursomc.domain.Cidade;
 import com.adriano.cursomc.domain.Cliente;
 import com.adriano.cursomc.domain.Endereco;
+import com.adriano.cursomc.domain.enums.Perfil;
 import com.adriano.cursomc.domain.enums.TipoCliente;
 import com.adriano.cursomc.dto.ClienteDTO;
 import com.adriano.cursomc.dto.ClienteNewDTO;
 import com.adriano.cursomc.repositories.ClienteRepository;
 import com.adriano.cursomc.repositories.EnderecoRepository;
+import com.adriano.cursomc.security.UserSS;
+import com.adriano.cursomc.services.exceptions.AuthorizationException;
 import com.adriano.cursomc.services.exceptions.DataIntegrityException;
 import com.adriano.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -31,10 +34,19 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repo;
+	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
+		
+		
+		UserSS user = UserService.authenticated();
+		if(user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
+	}
+		
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto näo encontrado! id: " + id + ",  Tipo: " + Cliente.class.getName()));
 	}
